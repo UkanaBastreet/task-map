a1f13f898be439e388a15a4cded96d1e268bdbe8
Ukana.Bastreet <s.ppp@bk.ru>
2024-04-28 18:03:28 +0300

    init react router dom + some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from "react";
 import "./App.css";
-import Input from "./components/ui/Input/Input";
+import { Link } from "react-router-dom";
+import { TaskList } from "./components/TaskList";
 
 function App() {
   return (
     <div className="App">
-      <Input />
+      <header>
+        <span>
+          <h1>Task Map</h1>
+        </span>
+        <span>
+          <Link to={"/auth"}>Sign In</Link>
+          <Link to={"/registration"}>Sign Up</Link>
+        </span>
+      </header>
+      <main>
+        <nav>nav</nav>
+        <section className="content">
+          <TaskList tasks={[]} />
+        </section>
+        <aside></aside>
+      </main>
     </div>
   );
 }
